7b7f34d3a44b9e15e31385b6ea289ae82704e59a
Markus Huang <markus@leansoftware.com.hk>
2013-07-14 18:32:42 +0800

    Add psql for create account and list tables etc basic operation

--- .emacs.el
+++ .emacs.el
@@ -1,3 +1,5 @@
+
+;; Bulit in mode turn on/off
 (global-hl-line-mode)
 (set-background-color "black")
 (set-foreground-color "white")
@@ -16,3 +18,33 @@
 		  "Edit .emacs customized setting file"
 		  (interactive) 
 		  (find-file "~/.emacs")))
+
+
+;;Self playful stuff
+(defun mark-comments ()
+  "Insert standard comment for ruby!"
+  (interactive)
+  (progn 
+    (insert "#==================================================")
+    (insert "# Ver   Date    Hist                               ")
+    (insert "#===================================================")))
+
+(global-set-key (kbd "C-c C-g") 'mark-comments)
+
+;; For loading inf-ruby extension
+;; curl -O https://raw.github.com/nonsequitur/inf-ruby/master/inf-ruby.el
+(load-file "~/.emacs.d/inf-ruby.el")
+
+(autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
+(autoload 'inf-ruby-setup-keybindings "inf-ruby" "" t)
+(eval-after-load 'ruby-mode
+  '(add-hook 'ruby-mode-hook 'inf-ruby-setup-keybindings))
+
+;; For loading yaml-mode.el
+;; curl -O https://raw.github.com/yoshiki/yaml-mode/master/yaml-mode.el
+(load-file "~/.emacs.d/yaml-mode.el")
+(require 'yaml-mode)
+
+;; Customize auto list session
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
--- memo-rails.md
+++ memo-rails.md
@@ -7,8 +7,28 @@
       password:
 
 Then create the user (AKA "role") inside PostgreSQL using psql.exe:
-
-    $ psql -d postgres
+    $sudo -u postgres psql #This use postgres account to run psql command
+    $psql -d postgres
     postgres=# create role app_name login createdb;
     postgres=# \q
+
+    #To list the database after rake db:create
+    $\list
+    $\c <db>
+    $\dt #List tables
+    $\dt #List tablespace
+
+
+To allow local access is via password authentication:
+Edit the file /etc/postgresql/9.1/main/pg_hba.conf 
+
+    CHANGE Line:
+    local   all             all                                     peer
+    To:
+    local   all             all                                     md5
+
+    sudo service postgresql restart
+
+If failed to create the app/user account that existed in postgres, you may see the error:
+    FATAL:  Peer authentication failed for user "<user>	 "
 
